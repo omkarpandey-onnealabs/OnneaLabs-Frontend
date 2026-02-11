72f0d38376b4bbc33ad589032f4e9f3cdc0d2ef2
omkarpandey-onnealabs <omkar.pandey@onnealabs.com>
2026-02-11 15:42:05 +0530

    Changes for smooth scroll

--- src/components/Navbar2.jsx
+++ src/components/Navbar2.jsx
@@ -1,7 +1,7 @@
 
 
 const MenuItems = [
-  { id: 1, name: "Client", link: "/client" },
+  { id: 1, name: "Client", link: "#client" },
   { id: 2, name: "Our Work", link: "#our-work" },
   { id: 3, name: "Services", link: "#services" },
   { id: 4, name: "Why Us", link: "/whyus" },
